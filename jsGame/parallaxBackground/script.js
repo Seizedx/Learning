6dd1d5cc18314395b34d711934629ab703f21f5b
Seizedx <mapedlow@hotmail.com>
2025-06-10 23:00:07 -0300

    g

---
+++ jsGame/parallaxBackground/script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 400; 
+const CANVAS_HEIGHT = canvas.height = 500;
+let gameSpeed = 5;
+
